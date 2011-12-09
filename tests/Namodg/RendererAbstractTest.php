97d54cd069d4c9575ad608ed3168416e3d7538b2
Maher Sallam <maher@sallam.me>
2011-12-09 11:33:13 +0100

    Add a few tests for RendererAbstract

---
+++ tests/Namodg/RendererAbstractTest.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once 'tests/TestHelper.php';
+
+// Used to tests methods' expectations as PHPUnit can't do it
+// with Abstract classes.
+class Renderer extends Namodg_RendererAbstract {
+  public function render() {}
+}
+
+class RendererAbstractTest extends NamodgTestCase {
+
+  protected $builder;
+
+  public function setUp() {
+    $this->builder = $this->getMockBuilder('Renderer')
+                          ->setConstructorArgs(array( 'input' ));
+
+    $this->subject = $this->getMockBuilder('Namodg_RendererAbstract')
+                          ->setConstructorArgs(array( 'input' ))
+                          ->getMockForAbstractClass(); 
+  }
+
+  public function testConstructorSetsTag() {
+    assertAttributeEquals('input', '_tag', $this->subject);
+  }
+
+  public function testGettingTag() {
+    assertEquals('input', $this->subject->getTag());
+  }
+
+  public function testAddingAttributes() {
+    $this->subject->addAttr('title', 'my-input');
+    $attrs = PHPUnit_Framework_Assert::readAttribute($this->subject, '_attrs');
+
+    assertArrayHasKey('title', $attrs);
+    assertEquals('my-input', $attrs['title']); 
+  }
+
+  public function testAddingIdAsAttrCallsTheIdAddingMethod() {
+    $mock =$this->builder->setMethods(array( 'setID' ))
+                         ->getMock();
+
+    $mock->expects($this->once())
+         ->method('setID');
+    
+    $mock->addAttr('id', 'my-input');
+  }
+}
