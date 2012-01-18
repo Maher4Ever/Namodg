f3ddc3dbfe0c704b564fd8820726bb0942a905a5
Maher Sallam <maher@sallam.me>
2012-01-18 11:41:47 +0100

    Fix paths in tests

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -1,13 +1,7 @@
 <?php
 
 // Autoload namodg libs
-function autoload_namodg_lib($lib) {
-  $path = 'lib/' . str_replace('_', '/', $lib) . '.php';
-  if ( file_exists($path) ) {
-    require_once $path;
-  }
-}
-spl_autoload_register('autoload_namodg_lib');
+require dirname(__FILE__) . '/../lib/namodg_init.php';
 
 // Load the assertion function to reduce the use of $this in tests
 require_once 'PHPUnit/Framework/Assert/Functions.php';
