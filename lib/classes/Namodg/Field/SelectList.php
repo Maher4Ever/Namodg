ceb3bedb9eb5e8c9b4762d15ac5b24ad7dc9293c
Maher Sallam <maher@sallam.me>
2012-02-01 00:41:19 +0100

    Implement the select list

---
+++ lib/classes/Namodg/Field/SelectList.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Namodg - Form Generator
+ * ========================
+ *
+ * Namodg is a class which allows to easily create, render, validate and process forms
+ *
+ * @author Maher Sallam <admin@namodg.com>
+ * @link http://namodg.com
+ * @copyright Copyright (c) 2010-2011, Maher Sallam
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   @license http://www.opensource.org/licenses/mit-license.php
+ *   @license http://www.gnu.org/licenses/gpl.html
+ */
+
+/**
+ * Namodg number field is used to reperesent
+ * a HTML select element in PHP.
+ *
+ * @package Namodg
+ * @subpackage Namodg_Field
+ */
+class Namodg_Field_SelectList extends Namodg_FieldAbstract {
+
+  /**
+   * Initialize the field.
+   *
+   * @param Namodg_Renderer_SelectListRenderer $renderer
+   * @param string $id
+   * @param array $metaData
+   */
+  public function __construct(Namodg_Renderer_SelectListRenderer $renderer, $id = NULL, array $metaData = array()) {
+    parent::__construct($renderer, $id, $metaData);
+  }
+
+  /**
+   * Returns a sanitized version of the value
+   * which safely can be saved in a database.
+   *
+   * @return string
+   */
+  public function getSanitizedValue() {
+    return filter_var($this->getValue(), FILTER_SANITIZE_STRING);
+  }
+
+}
---
+++ tests/Namodg/Field/SelectListTest.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'tests/TestHelper.php';
+
+class SelectListTest extends NamodgTestCase {
+
+  public function setUp() {
+    $this->subject = new Namodg_Field_SelectList(
+      new Namodg_Renderer_SelectListRenderer(
+        new DOMDocument('1.0')
+      ),
+      'rate'
+    );
+  }
+
+  public function testGettingSanitizedValues() {
+    $this->subject->setValue('<h1>test</h1>');
+
+    assertEquals(
+      'test',
+      $this->subject->getSanitizedValue()
+    );
+  }
+
+  public function testGettingHtml() {
+    $this->subject->setValue('5 stars')
+                  ->addValidation(new Namodg_Validation_RequiredValidation);
+
+    $result = <<<HTML
+<select name="rate" data-namodg-validation="required">
+  <option value="5 stars" selected>5 stars</option>
+</select>
+HTML;
+
+    assertEquals($this->_stripWhitespace($result), $this->subject->getHtml());
+  }
+
+}
